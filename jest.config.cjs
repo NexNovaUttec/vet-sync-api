a3072c1bc7eafacb86e23cf8bd52b0fffc15060a
Daniel <juandanielpel.85@gmail.com>
2026-02-19 20:51:49 -0600

    fix: mock database module in Jest to avoid Supabase init during tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {},
-  setupFiles: ['./jest.setup.cjs'],
   moduleNameMapper: {
+    '^#databases/index\\.js$': '<rootDir>/src/__mocks__/databaseMock.js',
     '^#models/(.*)$': '<rootDir>/src/models/$1',
     '^#schemas/(.*)$': '<rootDir>/src/schemas/$1'
   },
